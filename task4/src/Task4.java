7662c449c1f51f6478a10f65f22acf393e9a9ae2
Denvo <DenvoZonis@outlook.sg>
2025-10-07 19:23:16 +0800

    提交Task4

--- task4/src/Task4.java
+++ task4/src/Task4.java
@@ -8,15 +8,23 @@ public class Task4 {
      * @param n 汉诺塔层数，应为一个正整数
      */
     void hanoi(int n) {
+        hanoi0(n, 'A', 'B', 'C');
+    }
+
+    /**
+     * 用于递归计算汉诺塔的内部函数，计算汉诺塔应使用hanoi()函数而不应该使用此函数！
+     * @param n 汉诺塔层数
+     * @param primaryStake 初始柱编号
+     * @param temporaryStake 协助柱编号
+     * @param targetStake 目标柱编号
+     */
+    void hanoi0(int n, char primaryStake, char temporaryStake, char targetStake) {
         if (n == 1) {
-            System.out.println("A->C");
-        } else if (n == 2) {
-            System.out.println("A->B");
-            System.out.println("A->C");
-            System.out.println("B->C");
+            System.out.println(primaryStake + "->" + targetStake);
         } else {
-            hanoi(n - 2);
-            System.out.println("A->B");
+            hanoi0(n - 1, primaryStake, targetStake, temporaryStake);
+            System.out.println(primaryStake + "->" + targetStake);
+            hanoi0(n - 1, temporaryStake, primaryStake, targetStake);
         }
     }
 }
